d1f06c5c2eaa8aef99e9e3ca5b50b5639e53f31f
ajing <ajingnk@gmail.com>
2014-07-25 14:29:27 -0400

    second example

--- ReadML.py
+++ ReadML.py
@@ -18,17 +18,25 @@ def GetPeakbyMZRange(run, mzrange):
     print (max_intensity, max_mz, max_time)
 
 
+def PlotRange():
+    p = pymzml.plot.Factory()
+    n = 0
+    for spec in run:
+        n = n + 1
+        print n
+        p.newPlot()
+        p.add(spec.peaks, color=(200,00,00), style='circles')
+        p.add(spec.centroidedPeaks, color=(00,00,00), style='sticks')
+        p.add(spec.reprofiledPeaks, color=(00,255,00), style='circles')
+        p.save( filename="output/plotAspect.xhtml")
+        break
+
 if __name__ == "__main__":
-    run = pymzml.run.Reader("../E165ug.mzML", MSn_Precision = 250e-6)
-#    p = pymzml.plot.Factory()
-#    n = 0
-#    for spec in run:
-#        n = n + 1
-#        print n
-#        p.newPlot()
-#        p.add(spec.peaks, color=(200,00,00), style='circles')
-#        p.add(spec.centroidedPeaks, color=(00,00,00), style='sticks')
-#        p.add(spec.reprofiledPeaks, color=(00,255,00), style='circles')
-#        p.save( filename="output/plotAspect.xhtml")
-#        break
-    GetPeakbyMZRange(run, (1293.0, 1293.5))
+    # first example
+    #run = pymzml.run.Reader("../E165ug.mzML", MSn_Precision = 250e-6)
+    #GetPeakbyMZRange(run, (1293.0, 1293.5))
+    # second example
+    run = pymzml.run.Reader("../4tRNA1_102009.mzML", MSn_Precision = 250e-6)
+    GetPeakbyMZRange(run, (1402.0, 1402.5))
+    GetPeakbyMZRange(run, (1403.0, 1404))
+
